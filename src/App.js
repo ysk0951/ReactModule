352ab6c6a297964b0d41f42e8ca986f7d22baf5f
yskim-web <abcm556@naver.com>
2020-08-16 14:32:37 +0900

    #

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
 function App() {
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    fetch('/Spring/api.git')
+      .then(response => response.text())
+      .then(message => {
+        setMessage(message);
+      });
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App-title">{message}</h1>
       </header>
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+</p>
     </div>
-  );
+  )
 }
-
 export default App;
